208c6600077cfbf3f0838cdb3dba4043391cd767
KOVU9529 <bapgiordano@gmail.com>
2022-08-25 18:38:36 +0200

    collegamento per visualizzazione singolo fumetto

--- laravel-comics/resources/views/dettagli-fumetto.blade.php
+++ laravel-comics/resources/views/dettagli-fumetto.blade.php
@@ -3,7 +3,34 @@
 <div class="jumbotron">
 </div>
 <div class="blu">
+<img class="img-singola" src="{{ $current_comics['thumb'] }}" alt="{{ $current_comics['title'] }}">
 </div>
-<h1>{{ $current_comics['title'] }}</h1>
+<div class="info">
+    <div class="container">
+        <h1>{{ $current_comics['title'] }}</h1>
+        <div class="card-info">
+            <div class="card-text">
+                <div class="green-price">
+                    <div class="price">
+                        U.S. Price: {{ $current_comics['price'] }} <span>AVAILABLE</span>
+                    </div>
+                    <div class="available">
+                        Check Available
+                    </div>
+                </div>
+                <div class="description">
+                    <p class="description">
+                        {{ $current_comics['description'] }}
+                    </p>
+                </div>
+            </div>
+            <div class="card-img">
+                AVERTISEMENT
+                <img  src="{{ $current_comics['thumb'] }}" alt="{{ $current_comics['title'] }}">
+            </div>
+        </div>
+        
+    </div>
+</div>
     
 @endsection
--- laravel-comics/resources/views/fumetti.blade.php
+++ laravel-comics/resources/views/fumetti.blade.php
@@ -8,8 +8,12 @@
         <div class="card-flex">
             @foreach ($comics_array as $comics)
                 <div class="card">
-                    <img src="{{$comics['thumb']}}" alt="{{$comics['title']}}">
-                    <h3>{{$comics['title']}}</h3>
+                    <a href=" {{ route('dettagli_fumetto', [
+                        'id' => $comics['id']
+                    ]) }} ">
+                        <img src="{{$comics['thumb']}}" alt="{{$comics['title']}}">
+                        <h3>{{$comics['title']}}</h3>
+                    </a>
                 </div>
             @endforeach
         </div>
